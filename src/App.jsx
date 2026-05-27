9995589c5d190899f393ff4fc1c0422916d6733b
Aditya Saxena <aditya.saxena@dreamsoft4u.in>
2026-05-27 14:50:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import axios from "axios";
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>This is the frontend</h1>
+    </>
+  )
+}
+
+export default App
