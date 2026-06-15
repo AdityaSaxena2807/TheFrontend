ab820794922de7f600bd4270e1f03354a5f63858
Aditya Saxena <saxena.aditya.2807@gmail.com>
2026-06-15 14:22:52 +0530

    Remove unused SVG files for favicon and icons, and update Watch component TODO comment for clarity.

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -85,4 +85,4 @@ function Watch() {
 }
 
 export default Watch;
-//TODO complete this page with all apis and components. For now, it's just a skeleton with a video player and some placeholders for the rest of the content.
+//TODO complete this page with all apis and components. For now, it's just a skeleton with a video player and some placeholders for the rest of the content  
